840508d7df67ddf1e06f2057ca6b5beb9b0e4ca9
Ric Mershon <rmersh@ricmershon.com>
2020-07-01 23:14:17 -0400

    Added Header component and links to home and shop page.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import Home from './pages/Home/Home';
 import Shop from './pages/Shop/Shop';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={ Home } />
         <Route path='/shop' component={ Shop } />
